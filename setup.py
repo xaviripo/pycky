bf5a008c64a415f20e82342beb6637e11b61f99d
Xavier Ripoll <xaviripo97@gmail.com>
2020-01-18 00:37:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pycky',
+    version='0.1.0',
+    description='A simple unit testing framework for Python.',
+    url='http://github.com/xaviripo/pycky',
+    author='Xavier Ripoll',
+    author_email='xaviripo97@gmail.com',
+    license='MIT',
+    packages=['pycky'],
+    zip_safe=False
+)
