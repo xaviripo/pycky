fd5bfbcfad297b94a57f3a0bd005d8ff67d2e1bd
Xavier Ripoll <xaviripo97@gmail.com>
2020-01-19 15:30:08 +0100

    Import tests only from the given modules

--- bin/pycky.py
+++ bin/pycky.py
@@ -34,21 +34,13 @@ def main(scopes, printer, follow_imports):
 
     PYCKY = pycky.glb.PYCKY
 
-    # As we will run the imported code from Pycky, we'll be testing it.
-    # See /pycky/glb/__init__.py for more info.
-    PYCKY.testing = True
-
     PYCKY.modules = _process_scopes(scopes)
 
     PYCKY.printer = getattr(pycky.printers, printer)()
 
     PYCKY.follow_imports = follow_imports
 
-    # First import each given file as a module. This will define the decorated
-    # functions and thus "run" their decorators. These will load the tests into
-    # the PYCKY global inside the pycky.glb package.
-    for module in PYCKY.modules:
-        import_module(module)
+    _obtain_tests()
 
     # Now, try to run all the tests.
     for test in PYCKY.tests:
@@ -73,3 +65,22 @@ def _process_scopes(scopes):
         modules[module].add(inspectable)
 
     return modules
+
+def _obtain_tests():
+    """Imports the modules to extract the tests from in an isolated manner."""
+
+    PYCKY = pycky.glb.PYCKY
+
+    # As we will run the imported code from Pycky, we'll be testing it.
+    # See /pycky/glb/__init__.py for more info.
+    PYCKY.testing = True
+
+    # First import each given file as a module. This will define the decorated
+    # functions and thus "run" their decorators. These will load the tests into
+    # the PYCKY global inside the pycky.glb package.
+    for module in PYCKY.modules:
+        import_module(module)
+
+    # Disable the testing mode, so that we don't accidentally run tests on
+    # other modules we might import.
+    PYCKY.testing = False
